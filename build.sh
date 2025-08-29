a4efae145f5defabf7924ee8dffbacdedf82cef3
wlint <richohealthy@hotmail.com>
2025-08-29 17:24:19 +0800

    fix: 部署环境无法正常访问api

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 source /root/.cargo/env
 curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh
 export PATH="/root/.cargo/bin:$PATH"
-wasm-pack build --target web
+wasm-pack build --target bundler
 astro build
